ab98169c167045a84058125e94fc63ae46d5acee
Jorrit Poelen <jhpoelen+git@jhpoelen.nl>
2026-05-21 11:14:23 -0500

    add updated examples

---
+++ p3/examples/insect-pollinator-plant-associations-of-ireland.sql
@@ -0,0 +1,15 @@
+SELECT DISTINCT 
+  sourceTaxonFamilyName as pollinatorFamily, 
+  sourceTaxonName as pollinatorName,
+  targetTaxonFamilyName as plantFamily,
+  targetTaxonName as plantName,
+FROM 
+  'dist/euroappa-nuts-2021-col.parquet'
+WHERE
+  sourceTaxonPath ~ '.*[^A-Z]Insecta[ ].*'
+  AND sourceTaxonFamilyName NOT NULL 
+  -- Ireland Statistical Regions https://en.wikipedia.org/wiki/NUTS_statistical_regions_of_Ireland
+  -- NUTS Level 3 West: IE042 
+  AND NUTS_ID = 'IE042'
+GROUP BY sourceTaxonFamilyName, sourceTaxonName, targetTaxonFamilyName, targetTaxonName
+ORDER BY sourceTaxonFamilyName, targetTaxonFamilyName, sourceTaxonName, targetTaxonName;
---
+++ p3/examples/insect-pollinators-of-ireland.sql
@@ -0,0 +1,13 @@
+SELECT DISTINCT 
+  sourceTaxonFamilyName, 
+  sourceTaxonName 
+FROM 
+  'dist/euroappa-nuts-2021-col.parquet'
+WHERE
+  sourceTaxonPath ~ '.*[^A-Z]Insecta[ ].*'
+  AND sourceTaxonFamilyName NOT NULL 
+  -- Ireland Statistical Regions https://en.wikipedia.org/wiki/NUTS_statistical_regions_of_Ireland
+  -- NUTS Level 3 West: IE042 
+  AND NUTS_ID = 'IE042'
+GROUP BY sourceTaxonFamilyName, sourceTaxonName 
+ORDER BY sourceTaxonFamilyName, sourceTaxonName;
